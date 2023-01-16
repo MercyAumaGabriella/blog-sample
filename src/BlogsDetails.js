c04c459cfbf14d735ad80f0bcef50f544910f79f
Mercy Auma <aumagabriella@gmail.com>
2023-01-16 18:24:57 +0300

    sixth commit

--- src/BlogsDetails.js
+++ src/BlogsDetails.js
@@ -21,8 +21,9 @@ const BlogsDetails = () => {
 
     // using a custom Hook to get information
     const {data:blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
-    const history = useHistory();
 
+    const history = useHistory();
+    
     const handleDelete = () =>{
         fetch('http://localhost:8000/blogs/' + blog.id, {
             method: 'DELETE'
