26d35c433aa1e43e724c6e58472e336725b14a79
Mercy Auma <aumagabriella@gmail.com>
2023-01-23 19:30:59 +0300

    Seventh commit

--- src/popUp.js
+++ src/popUp.js
@@ -1,9 +1,26 @@
 import useState from "react";
+import { useParams , useHistory} from "react-router-dom";
+import useFetch from "./usefetch";
 
 const [Popp, showPopp] = useState(false);
 
 const showPoppUp = () => {
     showPopp(true);
+}
+
+const {id} = useParams();
+
+const {data:blog} = useFetch('http://localhost:8000/blogs/' + id);
+
+const history = useHistory();
+
+const goingToDelete = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, {
+        method: 'DELETE'
+    }). then(() => {
+        history.push('/');
+    })
+
 }
 
 const PopUpDelete = () => {
@@ -12,7 +29,7 @@ const PopUpDelete = () => {
             <div className="popUpDelete">
                 <h2>WARNING</h2>
                 <p>Are you sure you would like to delete this blog?</p>
-                <button>Yes</button>
+                <button onClick={goingToDelete}>Yes</button>
                 <button onClick={showPoppUp}>No</button>
             </div>
         </div>
